eaaad6a7bd2bbe01e532879b06ab64c0a6216ba0
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-08-26 19:46:30 -0400

    feat(header): implement header to page

--- frontend-dstaes/src/app/features/landing/pages/page-landing/page-landing.ts
+++ frontend-dstaes/src/app/features/landing/pages/page-landing/page-landing.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { LandingHeader } from "../../components/landing-header/landing-header";
+
 @Component({
   selector: 'page-landing',
-  imports: [],
+  imports: [LandingHeader],
   templateUrl: './page-landing.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
