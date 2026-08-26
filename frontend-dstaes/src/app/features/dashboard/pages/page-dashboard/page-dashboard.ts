71180b48815282439d981f0cd2d3afcd78b0ac1a
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-08-26 18:09:12 -0400

    feat(dashboard): create module and page

---
+++ frontend-dstaes/src/app/features/dashboard/pages/page-dashboard/page-dashboard.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'page-dashboard',
+  imports: [],
+  templateUrl: './page-dashboard.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PageDashboard { }
