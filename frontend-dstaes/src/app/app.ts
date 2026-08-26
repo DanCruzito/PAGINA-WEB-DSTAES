5b32f962965bd4e5f52fee5f1c5df5d8d6ee1c43
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-08-26 19:43:16 -0400

    feat: implement flowbite into main component

--- frontend-dstaes/src/app/app.ts
+++ frontend-dstaes/src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToggleThemeService } from './services/toggle-theme.service';
+import { UiMode } from './ui/ui-mode/ui-mode';
+//import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UiMode],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  private readonly theme_service = inject(ToggleThemeService);
   protected readonly title = signal('frontend-dstaes');
+
+  constructor() {
+    this.theme_service.initialize();
+    //initFlowbite();
+  }
 }
