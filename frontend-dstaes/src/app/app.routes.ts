6bfb61872fdf00d912800af5721066b261d9e383
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-08-26 18:09:34 -0400

    feat(routes): add landing page route and update routing configuration

--- frontend-dstaes/src/app/app.routes.ts
+++ frontend-dstaes/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PageLanding } from './features/landing/pages/page-landing/page-landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PageLanding
+  }
+];
