b1b3850106b1320f524f2c9aa9058d55956828eb
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-08-26 18:09:00 -0400

    feat(landing): create module and page

---
+++ frontend-dstaes/src/app/features/landing/pages/page-landing/page-landing.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'page-landing',
+  imports: [],
+  templateUrl: './page-landing.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PageLanding { }
