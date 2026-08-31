d118d107e335608b58715047c6097600819cc9d3
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-08-31 11:42:31 -0400

    feat(landing): add landing section start carousel component

---
+++ frontend-dstaes/src/app/features/landing/components/landing-section-start-carousel/landing-section-start-carousel.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-section-start-carousel',
+  imports: [],
+  templateUrl: './landing-section-start-carousel.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LandingSectionStartCarousel {}
---
+++ frontend-dstaes/src/app/features/landing/components/landing-section-start/landing-section-start.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-section-start',
+  imports: [],
+  templateUrl: './landing-section-start.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LandingSectionStart {}
