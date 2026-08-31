a9ba95af2c64c821c095213a919e7ee4e6c458ad
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-08-31 18:26:11 -0400

    feat(icons): add additional arrow icons to available icons list

--- frontend-dstaes/src/app/shared/ui/ui-icon/icons.ts
+++ frontend-dstaes/src/app/shared/ui/ui-icon/icons.ts
@@ -1,6 +1,11 @@
 export const available_icons = {
   sun: 'icon-[ri--sun-fill]',
   moon: 'icon-[ri--moon-fill]',
+  arrow_left: 'icon-[ri--arrow-left-s-line]',
+  arrow_right: 'icon-[ri--arrow-right-s-line]',
+  arrow_up: 'icon-[ri--arrow-up-s-line]',
+  arrow_down: 'icon-[ri--arrow-down-s-line]',
+
 
 } as const;
 
