8c24198dca93d4ce05398cd605bb608e223e5eed
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-08-31 20:16:17 -0400

    feat(landing): adjust layout and styles for landing section and carousel

--- frontend-dstaes/src/app/features/landing/components/landing-section-start/landing-section-start.ts
+++ frontend-dstaes/src/app/features/landing/components/landing-section-start/landing-section-start.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { LandingSectionStartCarousel } from "../landing-section-start-carousel/landing-section-start-carousel";
 import { UiButton } from "../../../../shared/ui/ui-button/ui-button";
 import { LandingCarouselService } from '../../services/landing-carousel.service';
+import { UiIcon } from "../../../../shared/ui/ui-icon/ui-icon";
 
 @Component({
   selector: 'landing-section-start',
-  imports: [LandingSectionStartCarousel, UiButton],
+  imports: [LandingSectionStartCarousel, UiButton, UiIcon],
   templateUrl: './landing-section-start.html',
   host: {
     class: 'block w-full'
