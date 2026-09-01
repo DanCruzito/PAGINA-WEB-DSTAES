900d99f0c4cd0cb3c471ad2da2ac3978e1e0a882
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-09-01 06:28:14 -0400

    feat(dashboard): add image components for application branding to dashboard component

--- frontend-dstaes/src/app/layout/layout-dashboard/components/dashboard-header/dashboard-header.ts
+++ frontend-dstaes/src/app/layout/layout-dashboard/components/dashboard-header/dashboard-header.ts
@@ -2,10 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { UiButton } from '../../../../shared/ui/ui-button/ui-button';
 import { UiMode } from '../../../../shared/ui/ui-mode/ui-mode';
+import { ImageIso } from "../../../../shared/components/image-iso/image-iso";
+import { ImageLogo } from "../../../../shared/components/image-logo/image-logo";
 
 @Component({
   selector: 'dashboard-header',
-  imports: [UiMode, UiButton, RouterLink],
+  imports: [UiMode, UiButton, RouterLink, ImageIso, ImageLogo],
   templateUrl: './dashboard-header.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
