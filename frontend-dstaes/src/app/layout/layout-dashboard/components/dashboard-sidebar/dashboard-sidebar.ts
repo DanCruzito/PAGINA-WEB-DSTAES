094748d223b7a34ea96e1e63d1dfa3005c9b96fb
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-09-01 05:45:23 -0400

    feat(dashboard) create sidebar component and integrate into layout

---
+++ frontend-dstaes/src/app/layout/layout-dashboard/components/dashboard-sidebar/dashboard-sidebar.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'dashboard-sidebar',
+  imports: [],
+  templateUrl: './dashboard-sidebar.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DashboardSidebar { }
--- frontend-dstaes/src/app/layout/layout-dashboard/template/layout-dashboard.ts
+++ frontend-dstaes/src/app/layout/layout-dashboard/template/layout-dashboard.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardHeader } from '../components/dashboard-header/dashboard-header';
+import { DashboardSidebar } from "../components/dashboard-sidebar/dashboard-sidebar";
 
 @Component({
   selector: 'layout-dashboard',
-  imports: [DashboardHeader, RouterOutlet],
+  imports: [DashboardHeader, RouterOutlet, DashboardSidebar],
   templateUrl: './layout-dashboard.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
