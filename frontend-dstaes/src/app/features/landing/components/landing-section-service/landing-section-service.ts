ec97c75002d727f93c608e765a4988e43158d15a
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2026-09-03 17:12:14 -0400

    feat(landing): enhance service section with detailed descriptions and updated images

--- frontend-dstaes/src/app/features/landing/components/landing-section-service/landing-section-service.ts
+++ frontend-dstaes/src/app/features/landing/components/landing-section-service/landing-section-service.ts
@@ -1,11 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { UiCard } from "../../../../shared/ui/ui-card/ui-card";
 import { UiIcon } from "../../../../shared/ui/ui-icon/ui-icon";
+import { landing_page_info } from '../../../../shared/data/landing_page_info';
+import { UiButton } from "../../../../shared/ui/ui-button/ui-button";
 
 @Component({
   selector: 'landing-section-service',
-  imports: [UiCard, UiIcon],
+  imports: [UiCard, UiIcon, UiButton],
   templateUrl: './landing-section-service.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LandingSectionService { }
+export class LandingSectionService {
+  services = landing_page_info.services;
+
+}
